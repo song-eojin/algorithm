0b1340a61e3ecdbc2e890f3d3255c9fb67000b47
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-01 16:02:40 +0900

    Update 문자열 출력하기.java

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,7 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {               Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println("문자열을 입력해주세요 ! ");
+        String str = br.readLine();
+
+        bw.newLine();
+        bw.write("문자열 출력 : " + str);
+
+        bw.flush();
+        br.close();
+        bw.close();
+
     }
 }
