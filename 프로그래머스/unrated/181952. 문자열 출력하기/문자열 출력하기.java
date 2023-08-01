957c9acede3e925e5c1d9bed029311e4db795c74
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-01 15:55:42 +0900

    [unrated] Title: 문자열 출력하기, Time: 161.77 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,16 +1,7 @@
-import java.io.*;
-
+import java.util.Scanner;
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String str = br.readLine();
-
-        bw.write(str);
-
-        bw.flush();
-        br.close();
-        bw.close();
+    public static void main(String[] args) {               Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
     }
 }
