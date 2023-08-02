f93d37e9d698faa62020aa50ecfe0c5a5882d7f2
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-02 15:41:52 +0900

    [unrated] Title: 문자열 돌리기, Time: 193.74 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/README.md
+++ 프로그래머스/unrated/181945. 문자열 돌리기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 문자열 돌리기 - 181945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181945?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181945) 
 
 ### 성능 요약
 
-메모리: 68.3 MB, 시간: 137.45 ms
+메모리: 60.8 MB, 시간: 193.74 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        
-        for(int i=0; i<a.length(); i++) {
-            System.out.println(a.charAt(i));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i)).append("\n");
         }
+        System.out.println(sb);
+        br.close();
     }
 }
