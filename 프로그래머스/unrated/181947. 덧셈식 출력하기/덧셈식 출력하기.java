d7205e9dc110b8a65b4f6f61fc92500cf8e881b1
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-02 14:50:27 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 161.12 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = a+b;
+
+        bw.write(a + " + " + b + " = " + c);
+        bw.close();
+    }
+}
