7c5af91e6c1a164e87c72ac08d13ae282c08ea20
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-02 15:44:48 +0900

    Update 문자열 돌리기.java

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -1,17 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
 
-        String str = br.readLine();
-        for (int i = 0; i < str.length(); i++) {
-            sb.append(str.charAt(i)).append("\n");
+        for(int i=0; i<a.length(); i++) {
+            System.out.println(a.charAt(i));
         }
-        System.out.println(sb);
-        br.close();
     }
 }
