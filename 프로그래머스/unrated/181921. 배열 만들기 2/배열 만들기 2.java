718363d1aae81938cb98828f9be374749da3448e
song eojin <122079064+song-eojin@users.noreply.github.com>
2023-08-08 22:00:38 +0900

    [unrated] Title: 배열 만들기 2, Time: 0.16 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 64; i++) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            if (l <= num && num <= r)
+                list.add(num);
+        }
+        return list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(i -> i).toArray();
+    }
+}
